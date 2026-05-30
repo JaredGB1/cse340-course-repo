a039b5755ce257033310fc8f0e3145b86b20be61
JaredGB1 <mjaredgb@gmail.com>
2026-05-29 19:49:40 -0600

    W03 Team Activity

--- src/models/projects.js
+++ src/models/projects.js
@@ -1,6 +1,6 @@
 import db from './db.js';
 
-const getAllProjects = async () => {
+const getAllProjects = async (number_of_projects) => {
     const query = `
         SELECT 
             sp.project_id,
@@ -9,7 +9,7 @@ const getAllProjects = async () => {
             sp.title,
             sp.description,
             sp.location,
-            sp.project_date
+            sp.project_date AS date
         FROM public.service_project sp
         JOIN public.organization o
             ON sp.organization_id = o.organization_id;
@@ -28,7 +28,7 @@ const getProjectsByOrganizationId = async (organizationId) => {
           title,
           description,
           location,
-          project_date
+          project_date AS date
         FROM service_project
         WHERE organization_id = $1
         ORDER BY project_date;
@@ -40,4 +40,51 @@ const getProjectsByOrganizationId = async (organizationId) => {
       return result.rows;
 };
 
-export { getAllProjects, getProjectsByOrganizationId };
+const getUpcomingProjects = async (number_of_projects) => {
+
+    const query = `
+        SELECT
+            sp.project_id,
+            sp.title,
+            sp.description,
+            sp.project_date AS date,
+            sp.location,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM public.service_project sp
+        JOIN public.organization o
+            ON sp.organization_id = o.organization_id
+        WHERE sp.project_date >= CURRENT_DATE
+        ORDER BY sp.project_date ASC
+        LIMIT $1;
+    `;
+
+    const queryParams = [number_of_projects];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+const getProjectDetails = async (projectId) => {
+    const query = `
+        SELECT
+            sp.project_id,
+            sp.title,
+            sp.description,
+            sp.project_date AS date,
+            sp.location,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM public.service_project sp
+        JOIN public.organization o
+            ON sp.organization_id = o.organization_id
+        WHERE sp.project_id = $1;
+    `;
+
+    const queryParams = [projectId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows[0];
+};
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
